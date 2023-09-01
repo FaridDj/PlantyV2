0043dc3a1f4451535401596e9e41e5e542729573
FaridDj <f.djaouti@hotmail.fr>
2023-09-01 17:13:59 +0200

    reponsive de la canette et des plantes

--- PlantyV2/wp-content/themes/blankslate-child/header.php
+++ PlantyV2/wp-content/themes/blankslate-child/header.php
@@ -14,30 +14,25 @@
 <?php wp_body_open(); ?>
 <div id="wrapper" class="hfeed">
 <header id="header" role="banner">
-<div id="branding">
-
-<div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-
-
+<div class=navigation>
+<div class=logo>
 <?php
-
-
 if ( function_exists( 'the_custom_logo' ) ) {
 	the_custom_logo();
 add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
 }
-
 ?>
 </div>
-<div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>></div>
-</div>
+
+
+<div>
 <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
-
-
-
-
 </nav>
+</div>
+
+</div>
+
 </header>
 <div id="container">
 <main id="content" role="main">
