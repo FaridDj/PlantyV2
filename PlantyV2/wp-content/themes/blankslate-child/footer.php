76a9a24427344ac15c7f1973fdd65af212db2501
FaridDj <f.djaouti@hotmail.fr>
2023-09-02 17:44:18 +0200

    mention legale footer

--- PlantyV2/wp-content/themes/blankslate-child/footer.php
+++ PlantyV2/wp-content/themes/blankslate-child/footer.php
@@ -2,8 +2,13 @@
 
 </div>
 <footer id="footer" role="contentinfo">
+
+
+
 <div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+<div class="mention"><a href="http://localhost/plantyV2/">Mentions légale</a></div>
+&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?>
+       <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
 </div>
 </footer>
 </div>
