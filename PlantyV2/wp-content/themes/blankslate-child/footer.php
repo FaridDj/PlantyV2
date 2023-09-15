b7d783d6c8e569e95df547b12c4625095b54e5ac
FaridDj <f.djaouti@hotmail.fr>
2023-09-15 10:35:25 +0200

    Projet 6 Finalisé - Planty- refonte du menu-suppression copyright-lien hover commander

---
+++ PlantyV2/wp-content/themes/blankslate-child/footer.php
@@ -0,0 +1,8 @@
+</main>
+<footer id="footer" role="contentinfo">
+<div class="mention"><a href="http://localhost/plantyV2/">Mentions légale</a></div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ PlantyV2/wp-content/themes/blankslate-child/header1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+<head>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Syne:wght@600;800&display=swap');
+</style>
+
+
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="wrapper" class="hfeed">
+<header id="header" role="banner">
+<div class=navigation>
+<div class=logo>
+<?php
+if ( function_exists( 'the_custom_logo' ) ) {
+	the_custom_logo();
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
+}
+?>
+</div>
+
+
+<div>
+<nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+</nav>
+</div>
+
+</header>
+<div id="container">
+<main id="content" role="main">
+</div>
